00e5a0c7d1a654256e5d98e963656c75af4c33bd
sijan <sijankasalawat3339@gmail.com>
2023-01-10 01:06:35 +0545

    register page is made

---
+++ flutter_application_1/lib/pages/register.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Register_Page extends StatefulWidget {
+  const Register_Page({super.key});
+
+  @override
+  State<Register_Page> createState() => _Register_PageState();
+}
+
+class _Register_PageState extends State<Register_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
